ce88f365382a38f33ed24cc2c727b559181cf5e3
Sendhil Panchadsaram <sendhilp@gmail.com>
2018-12-25 12:09:41 -0800

    Fixing background image script

--- scripts/setup-lockscreen-background.sh
+++ scripts/setup-lockscreen-background.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -e
 
-BACKGROUND_IMAGE=$(cat ~/.fehbg | grep feh  | cut -d "'" -f2)
-if [ -z "$BACKGROUND_IMAGE" ]
-then
-  betterlockscreen -u ~/DesktopBackgrounds/desktop
-else 
-  betterlockscreen -u $BACKGROUND_IMAGE
-fi
+BACKGROUND_IMAGE=$(ls ~/DesktopBackgrounds/desktop | shuf -n 1)
+~/.local/bin/betterlockscreen -u $BACKGROUND_IMAGE
