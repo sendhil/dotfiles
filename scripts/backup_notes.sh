076c26a470edd69916764298a6b6d87274c4ffff
Sendhil Panchadsaram <sendhilp@gmail.com>
2019-01-13 10:49:20 -0800

    Setting up backup scripts

---
+++ scripts/backup_notes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# Inspired from - https://medium.com/@fotios.floros/linux-backup-script-1722cc9c2bf6
+
+if [ ! -d "$HOME/Dropbox" ]; then
+  echo "Dropbox isn't setup"
+  exit 1
+fi
+
+mkdir -p "$HOME/Dropbox/Backups/general-notes"
+
+BACKUPTIME=$(date +%b-%d-%y)
+DESTINATION=$HOME/Dropbox/Backups/general-notes/general-notes-$BACKUPTIME.tar.gz
+SOURCEFOLDER=$HOME/Dropbox/general-notebook
+
+tar -cpzf "$DESTINATION" -C "$SOURCEFOLDER" .
