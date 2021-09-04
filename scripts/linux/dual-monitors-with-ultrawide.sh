43ab662753dd6ddb22244019ae18d7cd9d54a918
Sendhil Panchadsaram <sendhilp@gmail.com>
2021-09-03 20:58:35 -0700

    Re-organizing scripts folder
    Moved the linux scripts into the linux sub folder.

--- scripts/linux/dual-monitors-with-ultrawide.sh
+++ scripts/linux/dual-monitors-with-ultrawide.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --mode 3440x1440 --primary --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
 
-./reload-on-screen-change.sh
+./linux/reload-on-screen-change.sh
--- scripts/linux/single-display-ultrawide.sh
+++ scripts/linux/single-display-ultrawide.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --output DP-1 --mode 3440x1440 --pos 0x0 --rotate normal --output eDP-1 --off
 
-./reload-on-screen-change.sh
+./linux/reload-on-screen-change.sh
