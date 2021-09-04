e107d0e170e294838e9538407c79e799226d7af3
Sendhil Panchadsaram <sendhilp@gmail.com>
2021-09-03 22:22:38 -0700

    Adding MacOS scripts

---
+++ scripts/macos/switch-mouse-and-keyboard-profile.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+CLI_PATH = "/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
+
+BUILT_IN_PROFILE = "BuiltInKeyboard"
+EXTERNAL_PROFILE = "ExternalKeyboard"
+
+if BUILT_IN_PROFILE in subprocess.run([CLI_PATH, "--show-current-profile-name"], stdout=subprocess.PIPE).stdout.decode('utf-8'):
+    subprocess.run([CLI_PATH, "--select-profile", EXTERNAL_PROFILE])
+    subprocess.run(['./toggle-trackpad.sh'], stdout=subprocess.PIPE)
+    # subprocess.run(["defaults", "write", "NSGlobalDomain", "com.apple.swipescrolldirection", "-bool", "NO"])
+else:
+    subprocess.run([CLI_PATH, "--select-profile", BUILT_IN_PROFILE])
+    subprocess.run(['./toggle-trackpad.sh'], stdout=subprocess.PIPE)
+    # subprocess.run(["defaults", "write", "NSGlobalDomain", "com.apple.swipescrolldirection", "-bool", "YES"])
